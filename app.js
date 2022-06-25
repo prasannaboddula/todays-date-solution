565859f545e88815e39420228b2d50df758c7348
prasannaboddula <prasannaboddula749@gmail.com>
2022-06-25 11:17:48 +0000

    todays date

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/date", (request, response) => {
+  let date = new Date();
+  response.send(`Today's date is ${date}`);
+});
+
+app.listen(3000);
+module.exports = app;
